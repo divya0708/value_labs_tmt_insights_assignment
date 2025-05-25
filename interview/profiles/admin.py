b2b725cb89c5a11eb1d3b8f31159aeb66268d07f
unknown <divyadodwani@gmail.com>
2025-05-25 20:15:25 +0530

    Challenge-4-profile-model

---
+++ interview/profiles/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
+
+class CustomUserAdmin(UserAdmin):
+    model = UserProfile
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_admin')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_admin')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_superuser', 'is_admin')}
+        ),
+    )
+
+admin.site.register(UserProfile, CustomUserAdmin)
